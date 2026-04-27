c5e70c4c41c2a888f1157f3028068fa114e3bb1b
haseebkhanniazi76-art <haseebkhanniazi76@gmail.com>
2026-04-27 12:51:14 +0500

    project 2

--- project 2.py
+++ project 2.py
@@ -1,13 +1,21 @@
 import pandas as pd
 df=pd.read_csv("morning_stock.csv")
 class Ingredient:
-    def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = float(quantity)
+    def __init__(self,ingredient,Qty_kg,cost_per_kg):
+        self.ingredient = ingredient
+        self.Qty_kg = Qty_kg
+        self.cost_per_kg=cost_per_kg
+        
 
     def use_item(self, amount):
-        if amount <= self.quantity:
-            self.quantity -= amount
+        if amount <= self.Qty_kg
+            self.Qty_kg -= amount
         else:
             print(f"Not enough {self.name} in stock!")
-print(df)
+df.rename(column={"Qty_kg" : "current_quantity"})
+cofee_row=df[df["ingredient"]=="cofee_beans["current_Quantity"].iloc[0]
+beans_object=ingredient(cofee_row["ingredient"],cofee_row["current_quantity])
+beans_object.use_items(2.5)
+df.loc[df["ingredient"]=="cofee_beans."current_Quantity"]=beans_Object.quantity
+df.to_csv("evening_Stock.csv",index=false)
+
